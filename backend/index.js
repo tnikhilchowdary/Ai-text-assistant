cba607c2e2b524b5b3372e8af29cff1b91224c8d
THUMMALAPALLI NIKHIL CHOWDARY <tnikhilchowdary@gmail.com>
2026-02-10 18:27:54 -0600

    OPENAPI Created

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,24 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import OpenAI from "openai"
 
+dotenv.config();
+
+const app = express();
+const PORT = 5000;
+
+app.use(cors());
+app.use(express.json());
+
+const openai = new OpenAI({
+    apiKey:process.env.OPEN_API_KEY,
+});
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+})
+
+app.listen(PORT, () => {
+    console.log("Server is running on port ${PORT}")
+})
